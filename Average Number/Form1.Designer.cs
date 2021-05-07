34c1a63b80a340292356b47f7235fa07efceb5f8
trenmcki071 <trenmcki071@outlook.com>
2021-05-07 10:39:16 -0400

    Add project files.

---
+++ Average Number/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Average_Number
+{
+    partial class averageValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.findAverage = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-3, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(391, 52);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Average Number Solver";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(55, 81);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(267, 23);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = "Enter a number and press add";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(110, 137);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(73, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(189, 135);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // findAverage
+            // 
+            this.findAverage.Location = new System.Drawing.Point(88, 297);
+            this.findAverage.Name = "findAverage";
+            this.findAverage.Size = new System.Drawing.Size(95, 34);
+            this.findAverage.TabIndex = 4;
+            this.findAverage.Text = "Find average";
+            this.findAverage.UseVisualStyleBackColor = true;
+            this.findAverage.Click += new System.EventHandler(this.findAverage_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputLabel.Location = new System.Drawing.Point(12, 184);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(364, 97);
+            this.outputLabel.TabIndex = 5;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(189, 297);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(95, 34);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // averageValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 343);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.findAverage);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "averageValue";
+            this.Text = "Average Values";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button findAverage;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Average Number/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Average_Number
+{
+    public partial class averageValue : Form
+    {
+        public averageValue()
+        {
+            InitializeComponent();
+        }
+        double numberOfNumbers = 0;
+        double totalValue = 0;
+        double textBoxNumber;
+        double average;
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxNumber = Int32.Parse(textBox.Text);
+                totalValue = totalValue + textBoxNumber;
+                numberOfNumbers = numberOfNumbers + 1;
+                instructionLabel.Text = "Enter a number and press add";
+                textBox.Text = "";
+                outputLabel.Text = $"{textBoxNumber} was added to the sum";
+            }
+            catch { instructionLabel.Text = "ENTER A NUMBER"; }
+        }
+
+        private void findAverage_Click(object sender, EventArgs e)
+        {
+            average = totalValue / numberOfNumbers;
+            outputLabel.Text = $"{average} is your average";
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = "";
+            totalValue = 0;
+            average = 0;
+            numberOfNumbers = 0;
+            textBox.Text = "";
+        }
+    }
+}
